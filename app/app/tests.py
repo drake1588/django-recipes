6d00b64fec64088ef86be63daef5482ba2318dcf
Alfonso Henriquez <alfo704@gmail.com>
2022-08-01 11:53:17 -0400

    Fix lint and add test

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,10 +5,15 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
-
     def test_add_numbers(self):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        res = calc.substract(6, 5)
+
+        self.assertEqual(res, 1)
