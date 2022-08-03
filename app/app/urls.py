0288bcccc12a3c7fb61afb4d98bb74d7cf68cff6
Alfonso Henriquez <alfo704@gmail.com>
2022-08-03 18:49:50 -0400

    Add recipes endpoints

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,10 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import (
+    path,
+    include,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +31,6 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
-    path('api/user', include('user.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
